5f5ba99f97d706a2ff5a663f7e8ce0a7928dc88c
Kalipso <kurbanovmurad13@gmail.com>
2024-03-18 07:04:33 +0300

    second commit

--- TestGenerate/Program.cs
+++ TestGenerate/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestGenerate.Model;
 
 namespace TestGenerate
 {
@@ -10,6 +11,10 @@ namespace TestGenerate
     {
         static void Main(string[] args)
         {
+            using(var db = new dbModel())
+            {
+                Console.WriteLine("тест");
+            }
         }
     }
 }
